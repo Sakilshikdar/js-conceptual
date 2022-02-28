01dffc47a35e66e134a320ddb6b44840b10955f9
sakilshikdar <sakilshikdar2504@gmail.com>
2022-02-28 09:40:07 -1200

    good

--- js/app.js
+++ js/app.js
@@ -1,9 +1,9 @@
-
-const loadDog =() => {
-fetch("https://api.thedogapi.com/v1/breeds")
-.then(res => res.json())
-.then(data => displayDog(data))
-}
-const displayDog = (doglist) => {
-    console.log(doglist)
+const allPlayer =() =>{
+    const searchValue = document.getElementById('search-box').value;
+    const url = `https://www.thesportsdb.com/api/v1/json/2/searchplayers.php?p=${searchValue}`
+    // console.log(url)
+    fetch(url)
+    .then(res => res.json())
+    .then(data => console.log(data));
+    console.log(data)
 }
